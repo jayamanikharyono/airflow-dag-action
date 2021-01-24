dbfb5cddac41f42e8cf49e0d938fcc9706dbc42b
jayamanikharyono <jayaharyonomanik@gmail.com>
2021-01-24 16:05:34 +0700

    update env var

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,9 @@ RUN pip install google-auth-httplib2
 RUN pip install google-api-python-client
 RUN pip install pandas-gbq
 
-ADD requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
 
-ENV AIRFLOW_HOME=/github/workspace/airflow
-ENV PYTHONPATH "${PYTHONPATH}:${AIRFLOW_HOME}"
+#ENV AIRFLOW_HOME=/github/workspace/airflow
+#ENV PYTHONPATH "${PYTHONPATH}:${AIRFLOW_HOME}"
 
 RUN chmod +x /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
-echo "Start Testing"
-echo "Var file : $1"
-echo "DAGs dir : $2"
 
-sed -i "s/{{dags}}/$2/" dag_validation.py
+echo "Start Testing"
+echo "Req path : $1"
+echo "Var path : $2"
+echo "DAGs dir : $3"
+
+#ENV AIRFLOW_HOME=/github/workspace/airflow
+#ENV PYTHONPATH "${PYTHONPATH}:${AIRFLOW_HOME}"
+
+export VARNAME="/github/workspace/$3"
+export PYTHONPATH="${PYTHONPATH}:${AIRFLOW_HOME}"
+
+pip install -r $1
+
+sed -i "s/{{dags}}/$3/" dag_validation.py
+
+cat dag_validation.py
 
 airflow initdb > /dev/null
 
-airflow variables --import $1
+airflow variables --import $2
 
 pytest dag_validation.py
