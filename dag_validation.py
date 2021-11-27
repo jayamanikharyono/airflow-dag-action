96cd62f40d803f4bb106865b9de5d9851a95a45c
jayamanikharyono <jayaharyonomanik@gmail.com>
2021-11-27 17:09:41 +0700

    update dagbag report

--- dag_validation.py
+++ dag_validation.py
@@ -13,7 +13,7 @@ class TestDagIntegrity(unittest.TestCase):
     def setUp(self):
         DAGS_DIR = os.environ['INPUT_DAGPATHS']
         os.environ['PYTHONPATH'] = f"{os.getenv('PYTHONPATH')}:{DAGS_DIR}"
-        sys.stderr.write("DAGs dir : {}".format(DAGS_DIR))
+        sys.stdout.write("DAGs dir : {}".format(DAGS_DIR))
         self.dagbag = DagBag(dag_folder = DAGS_DIR, include_examples = False)
         #print(self.dagbag.dagbag_report())
 
@@ -24,7 +24,8 @@ class TestDagIntegrity(unittest.TestCase):
                 self.dagbag.import_errors
             )
         )
-        sys.stderr.write(self.dagbag.dagbag_report())
+
+    sys.stdout.write(self.dagbag.dagbag_report())
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDagIntegrity)
 unittest.TextTestRunner(verbosity=1).run(suite)
