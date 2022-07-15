69564180e96682f623794a0bd7693095d6c0cfb0
kieronellis <69465049+kieronellis@users.noreply.github.com>
2022-07-15 15:14:12 +0200

    Exit 1 if Pytest does not exit 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,8 @@ airflow variables import $3
 cp -r /action/* /github/workspace/
 
 pytest dag_validation.py -s -q >> result.log
+pytest_exit_code=`echo Pytest exited $?`
+echo $pytest_exit_code
 python alert.py --log_filename=result.log --repo_token=$4
+if [ "$pytest_exit_code" != "Pytest exited 0" ]; then echo "Pytest did not exit 0" ;fi
+if [ "$pytest_exit_code" != "Pytest exited 0" ]; then exit 1 ;fi
